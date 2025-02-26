886c4e8b2102b29085e8870407673891595e8dab
David Ireoluwa Akins (aka AwesomDev) <gooddavvy@gmail.com>
2025-02-26 16:00:52 -0600

    Add temp-branch-1.7

---
+++ example/output/foo_{{ componentType }}.js
@@ -0,0 +1,27 @@
+/**
+ * DataProcessor Service
+ * Service for processing data streams
+ */
+
+class DataProcessorService {
+  constructor(options = {}) {
+    this.name = 'DataProcessor';
+    this.type = 'Service';
+    this.options = options;
+  }
+
+  initialize() {
+    console.log(`Initializing ${this.name} ${this.type}`);
+    this.cache = new Map();
+  }
+
+  process(data) {
+    return {
+      processed: true,
+      result: data.map(item => ({ ...item, processed: true })),
+      timestamp: new Date()
+    };
+  }
+}
+
+module.exports = DataProcessorService;
